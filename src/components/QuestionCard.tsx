1ee37a5d0413622152c375675bac45b94f6b8b61
AB <austinbuhler@gmail.com>
2020-12-08 16:21:33 -0500

    Initial setup

---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+type Props = {
+  question: string
+  answers: string[]
+  callback: any
+  userAnswer: boolean
+  questionNum: number
+  totalQuestions: number
+}
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNum,
+  totalQuestions
+}) => (
+  <div>
+    <p className='number'>
+      Question: {questionNum} / {totalQuestions}
+    </p>
+    {/* <p dangerouslySetInnerHTML={{ __html: question }} /> */}
+    <p>{question}</p>
+    <div>
+      {answers.map((answer) => (
+        <div>
+          <button disabled={userAnswer} onClick={callback}>
+            {/* <span dangerouslySetInnerHTML={{__html: answer}} /> */}
+            <span>{answer}</span>
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+)
+
+export default QuestionCard
